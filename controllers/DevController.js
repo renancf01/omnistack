5db0b2f996de331c074904faf3efd471c77657c2
renancf01 <renancf01@gmail.com>
2020-02-13 23:37:29 -0300

    controllers

---
+++ controllers/DevController.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const Dev = require('../models/dev');
+
+module.exports = {
+
+async store (req, res) {
+    const { github_username, techs } = req.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio, latitude, longitude } = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    };
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        tech: techsArray,
+        location,
+        
+    });
+
+    return res.json(dev);
+
+    }
+};
